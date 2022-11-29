8707b6f305f475714b5c2f865a2105bea490c6f0
Omar Ehab <topdollar57@gmail.com>
2022-11-29 16:09:41 +0200

    fix error

--- app/Http/Controllers/Admin/CertificatesController.php
+++ app/Http/Controllers/Admin/CertificatesController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateCertificateRequest;
 use App\Http\Requests\UpdateCertificateRequest;
 use App\Models\Certificate;
-use App\Services\ImageService;
+use App\services\ImageService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
