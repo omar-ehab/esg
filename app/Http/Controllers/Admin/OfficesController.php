7915761ef3d874a3f71e18dd020f675c20430d64
Omar Ehab <topdollar57@gmail.com>
2022-11-30 22:29:14 +0200

    fix error

--- app/Http/Controllers/Admin/OfficesController.php
+++ app/Http/Controllers/Admin/OfficesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\OfficeRequest;
 use App\Models\Office;
-use App\Services\ImageService;
+use App\services\ImageService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
