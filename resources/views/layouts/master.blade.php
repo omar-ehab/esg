9a9d48b023d09e22a31ffd52c8e16bf7d18f97fe
Omar Ehab <topdollar57@gmail.com>
2022-12-14 18:13:16 +0200

    adding favicons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,19 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module FrontEnd</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Module FrontEnd</title>
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/frontend.css') }}"> --}}
-
-    </head>
-    <body>
-        @yield('content')
-
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/frontend.js') }}"></script> --}}
-    </body>
+</head>
+<body>
+@yield('content')
+</body>
 </html>
